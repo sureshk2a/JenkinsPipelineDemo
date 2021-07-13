175611f41ff0544c4b7d2a38c2b6ca8ec73c1849
Suresh Kumar <34769206+sureshk2a@users.noreply.github.com>
2021-07-13 11:45:24 +0530

    Updated from script pipeline to descriptive pipe

--- nodejs_Jenkinsfile.groovy
+++ nodejs_Jenkinsfile.groovy
@@ -1,45 +1,40 @@
 pipeline {
 
     agent any
-    currentBuild.result = "SUCCESS"
-    
     stages {
-      try {
        stage('Test'){
-
-         env.NODE_ENV = "test"
-
-         print "Environment will be : ${env.NODE_ENV}"
-
-         bat 'node -v'
-         bat 'npm prune'
-         bat 'npm install'
-         bat 'npm test'
-       }
+          steps{
+            script {
+              try {
+                bat 'node -v'
+                bat 'npm prune'
+                bat 'npm install'
+                bat 'npm test'
+              } catch (Exception e) {
+                  echo 'Exception occurred: ' + e.toString()
+                  mail body: "Jenkins - Build Error: ${env.BUILD_URL}" ,
+                        from: 'suresha@ilink-systems.com.com',
+                        replyTo: 'sureshkumar_a@outlook.com',
+                        subject: 'Jenkins - Build failed',
+                        to: 'sureshkumar_a@outlook.com'
+                      throw e
+              }
+            }
+          }
+    }
 
        stage('Cleanup'){
-         echo 'prune and cleanup'
-         bat 'npm prune'
-         bat 'rm node_modules -rf'
+         steps{
+          echo 'prune and cleanup'
+          bat 'npm prune'
+          bat 'rm node_modules -rf'
 
-         mail body: 'Jenkins - Build Successful',
+          mail body: 'Jenkins - Build Successful',
               from: 'suresha@ilink-systems.com.com',
               replyTo: 'sureshkumar_a@outlook.com',
               subject: 'Jenkins - Build Passed',
               to: 'sureshkumar_a@outlook.com'
-
-       }
-    }
-    catch (err) {
-        currentBuild.result = "FAILURE"
-
-            mail body: "Jenkins - Build Error: ${env.BUILD_URL}" ,
-            from: 'suresha@ilink-systems.com.com',
-            replyTo: 'sureshkumar_a@outlook.com',
-            subject: 'Jenkins - Build failed',
-            to: 'sureshkumar_a@outlook.com'
-
-        throw err
+         }
       }
-    }
+  }
 }
