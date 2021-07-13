c17446e0a07f18c953cf055b676e36424532bedb
Suresh Kumar <34769206+sureshk2a@users.noreply.github.com>
2021-07-13 11:26:06 +0530

    Added main file with test script

---
+++ nodejs_Jenkinsfile.groovy
@@ -0,0 +1,44 @@
+node('node') {
+
+  
+    currentBuild.result = "SUCCESS"
+
+    try {
+       stage('Test'){
+
+         env.NODE_ENV = "test"
+
+         print "Environment will be : ${env.NODE_ENV}"
+
+         bat 'node -v'
+         bat 'npm prune'
+         bat 'npm install'
+         bat 'npm test'
+       }
+
+       stage('Cleanup'){
+         echo 'prune and cleanup'
+         sh 'npm prune'
+         sh 'rm node_modules -rf'
+
+         mail body: 'Jenkins - Build Successful',
+              from: 'suresha@ilink-systems.com.com',
+              replyTo: 'sureshkumar_a@outlook.com',
+              subject: 'Jenkins - Build Passed',
+              to: 'sureshkumar_a@outlook.com'
+
+       }
+    }
+    catch (err) {
+        currentBuild.result = "FAILURE"
+
+            mail body: "Jenkins - Build Error: ${env.BUILD_URL}" ,
+            from: 'suresha@ilink-systems.com.com',
+            replyTo: 'sureshkumar_a@outlook.com',
+            subject: 'Jenkins - Build failed',
+            to: 'sureshkumar_a@outlook.com'
+
+        throw err
+    }
+
+}
