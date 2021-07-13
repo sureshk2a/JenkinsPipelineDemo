8693791628094059d3d53279cbf11896056b28d3
Suresh Kumar <34769206+sureshk2a@users.noreply.github.com>
2021-07-13 14:01:04 +0530

    added mailer in groovy script

--- nodejs_Jenkinsfile.groovy
+++ nodejs_Jenkinsfile.groovy
@@ -5,35 +5,29 @@ pipeline {
        stage('Test'){
           steps{
             script {
-              try {
                 bat 'node -v'
                 bat 'npm prune'
                 bat 'npm install'
                 bat 'npm test'
-              } catch (Exception e) {
-                  echo 'Exception occurred: ' + e.toString()
-                  mail body: "Jenkins - Build Error: ${env.BUILD_URL}" ,
-                        from: 'suresha@ilink-systems.com.com',
-                        replyTo: 'sureshkumar_a@outlook.com',
-                        subject: 'Jenkins - Build failed',
-                        to: 'sureshkumar_a@outlook.com'
-                      throw e
+                script {
+                  def mailRecipients = 'sureshkumar_a@outlook.com'
+                  def jobName = currentBuild.fullDisplayName
+                  emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+                  from : 'suresha@ilink-systems.com',
+                  mimeType: 'text/html',
+                  subject: "[Jenkins] ${jobName}",
+                  to: "${mailRecipients}",
+                  replyTo: "${mailRecipients}",
+                  recipientProviders: [[$class: 'CulpritsRecipientProvider']]
               }
             }
-          }
+        }
     }
 
        stage('Cleanup'){
          steps{
           echo 'prune and cleanup'
           bat 'npm prune'
-          bat 'rm node_modules -rf'
-
-          mail body: 'Jenkins - Build Successful',
-              from: 'suresha@ilink-systems.com.com',
-              replyTo: 'sureshkumar_a@outlook.com',
-              subject: 'Jenkins - Build Passed',
-              to: 'sureshkumar_a@outlook.com'
          }
       }
   }
